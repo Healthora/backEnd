746b07c72405ec4fc72da6799e030d2fd2799ecd
Issam Kebdani <kebdaniissam780@gmail.com>
2026-03-21 21:58:02 +0000

    fix(db): utilisation de MYSQL_URL pour railway

--- database.js
+++ database.js
@@ -3,15 +3,17 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const pool = process.env.MYSQL_URL 
+  ? mysql.createPool(process.env.MYSQL_URL)
+  : mysql.createPool({
+      host: process.env.MYSQLHOST,
+      user: process.env.MYSQLUSER,
+      password: process.env.MYSQLPASSWORD,
+      database: process.env.MYSQLDATABASE,
+      port: process.env.MYSQLPORT,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    });
 
 export default pool;
