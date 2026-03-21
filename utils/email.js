87a11cee3083e843c00e7d7c411710f016cdc003
Issam Kebdani <kebdaniissam780@gmail.com>
2026-03-21 20:15:36 +0000

    add app passwird

--- utils/email.js
+++ utils/email.js
@@ -6,7 +6,7 @@ const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER || 'kebdaniissam780@gmail.com',
-        pass: process.env.EMAIL_PASS || 'ISS+N2327'
+        pass: process.env.EMAIL_PASS || 'iadulhwskdohjqsh'
     }
 });
 
